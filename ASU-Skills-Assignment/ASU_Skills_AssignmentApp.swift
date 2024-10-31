8c902d6db6af2106500a38374f23da40cd52f37e
Preston <101310062+colorfulparadox@users.noreply.github.com>
2024-10-30 22:18:04 -0700

    Initial Commit

---
+++ ASU-Skills-Assignment/ASU_Skills_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASU_Skills_AssignmentApp.swift
+//  ASU-Skills-Assignment
+//
+//  Created by Preston Wilson on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ASU_Skills_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
